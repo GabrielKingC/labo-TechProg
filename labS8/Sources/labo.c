a7420fe79350e91794a0daa3bb2cee45846d19bb
WilliamPronovost <2430066@bart.ca.com>
2025-04-01 11:56:41 -0400

    Remise labo S8

--- labS8/Sources/labo.c
+++ labS8/Sources/labo.c
@@ -1,1 +1,35 @@
 #include "labo.h"
+
+
+long long fibonacci(int n, int* tableau) {
+	if (tableau[n] == 0) {
+		return 0;
+	}
+	return tableau[n] + additionTableau(tableau, n + 1);
+}
+
+TreeNode* tree_create_node(void* data){
+		TreeNode* t = t->data;
+		if(t->data == NULL){
+		return 1;
+		}
+		return t->data; 
+}
+
+void tree_add_node(TreeNode* root, TreeNode* node){
+
+	if(root->left && root->right == NULL){
+		node->left;
+	}
+	else{
+		return 0;
+	}
+}
+
+int dfs(TreeNode* root, void* key){
+
+}
+
+int bfs(TreeNode* root, void* key){
+
+}
